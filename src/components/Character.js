ba61f70e6e313832d4e683376972ee842029a5f6
sunyang9876 <sunyang9876@hotmail.com>
2020-01-28 14:01:54 +1000

    hook

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,85 +1,113 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHttp } from './../hooks/http';
 
 import Summary from './Summary';
 
-class Character extends Component {
-  state = { loadedCharacter: {}, isLoading: false };
+const Character = props => {
+  // state = { loadedCharacter: {}, isLoading: false };
+  // const [loadedCharacter, setLoadedCharacter] = useState({});
+  // const [isLoading, setIsLoading] = useState(false);
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate');
-    return (
-      nextProps.selectedChar !== this.props.selectedChar ||
-      nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
-      nextState.isLoading !== this.state.isLoading
-    );
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('shouldComponentUpdate');
+  //   return (
+  //     nextProps.selectedChar !== props.selectedChar ||
+  //     nextState.loadedCharacter.id !== loadedCharacter.id ||
+  //     nextState.isLoading !== isLoading
+  //   );
+  // }
+
+  const [isLoading, fetchedData] = useHttp('https://swapi.co/api/people/' + props.selectedChar, [props.selectedChar]);
+  let loadedCharacter = null;
+  if (fetchedData) {
+    loadedCharacter = {
+      id: props.selectedChar,
+      name: fetchedData.name,
+      height: fetchedData.height,
+      colors: {
+        hair: fetchedData.hair_color,
+        skin: fetchedData.skin_color
+      },
+      gender: fetchedData.gender,
+      movieCount: fetchedData.films.length
+    };
   }
 
-  componentDidUpdate(prevProps) {
-    console.log('Component did update');
-    if (prevProps.selectedChar !== this.props.selectedChar) {
-      this.fetchData();
-    }
-  }
+  // const fetchData = () => {
+  //   console.log(
+  //     'Sending Http request for new character with id ' +
+  //       props.selectedChar
+  //   );
+  //   setIsLoading(true);
+  //   fetch('https://swapi.co/api/people/' + props.selectedChar)
+  //   .then(response => {
+  //     if (!response.ok) {
+  //       throw new Error('Could not fetch person!');
+  //     }
+  //     return response.json();
+  //   })
+  //   .then(charData => {
+  //     const loadedCharacter = {
+  //       id: props.selectedChar,
+  //       name: charData.name,
+  //       height: charData.height,
+  //       colors: {
+  //         hair: charData.hair_color,
+  //         skin: charData.skin_color
+  //       },
+  //       gender: charData.gender,
+  //       movieCount: charData.films.length
+  //     };
+  //     setIsLoading(false);
+  //     setLoadedCharacter(loadedCharacter);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     setIsLoading(false);
+  //   });
+  // };
 
-  componentDidMount() {
-    this.fetchData();
-  }
+  // // useEffect(() => {
+  // //   fetchData();
+  // // }, [])
 
-  fetchData = () => {
-    console.log(
-      'Sending Http request for new character with id ' +
-        this.props.selectedChar
-    );
-    this.setState({ isLoading: true });
-    fetch('https://swapi.co/api/people/' + this.props.selectedChar)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Could not fetch person!');
-        }
-        return response.json();
-      })
-      .then(charData => {
-        const loadedCharacter = {
-          id: this.props.selectedChar,
-          name: charData.name,
-          height: charData.height,
-          colors: {
-            hair: charData.hair_color,
-            skin: charData.skin_color
-          },
-          gender: charData.gender,
-          movieCount: charData.films.length
-        };
-        this.setState({ loadedCharacter: loadedCharacter, isLoading: false });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+  // useEffect(() => {
+  //   fetchData();
+  // }, [props.selectedChar])
 
-  componentWillUnmount() {
-    console.log('Too soon...');
-  }
+  // componentDidUpdate(prevProps) {
+  //   console.log('Component did update');
+  //   if (prevProps.selectedChar !== props.selectedChar) {
+  //     this.fetchData();
+  //   }
+  // };
 
-  render() {
+
+  
+
+  // componentWillUnmount() {
+  //   console.log('Too soon...');
+  // }
+
     let content = <p>Loading Character...</p>;
 
-    if (!this.state.isLoading && this.state.loadedCharacter.id) {
+    if (!isLoading && loadedCharacter) {
       content = (
         <Summary
-          name={this.state.loadedCharacter.name}
-          gender={this.state.loadedCharacter.gender}
-          height={this.state.loadedCharacter.height}
-          hairColor={this.state.loadedCharacter.colors.hair}
-          skinColor={this.state.loadedCharacter.colors.skin}
-          movieCount={this.state.loadedCharacter.movieCount}
+          name={loadedCharacter.name}
+          gender={loadedCharacter.gender}
+          height={loadedCharacter.height}
+          hairColor={loadedCharacter.colors.hair}
+          skinColor={loadedCharacter.colors.skin}
+          movieCount={loadedCharacter.movieCount}
         />
       );
-    } else if (!this.state.isLoading && !this.state.loadedCharacter.id) {
+    } else if (!isLoading && !loadedCharacter) {
       content = <p>Failed to fetch character.</p>;
     }
     return content;
-  }
 }
 
-export default Character;
+export default React.memo(Character, (prevProps, nextProps) => {
+  return nextProps.selectedChar === prevProps.selectedChar
+});
---
+++ src/hooks/http.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+export const useHttp = (url,dependencies) => {
+    const [fetchedData,setFetchedData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    // fetch('https://swapi.co/api/people')
+    useEffect(() => {
+        setIsLoading(true);
+        fetch(url)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Failed to fetch.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            setIsLoading(false);
+            setFetchedData(data);
+            
+        })
+        .catch(err => {
+            console.log(err);
+            setIsLoading(false);
+        });        
+    }, dependencies)
+
+
+    return [isLoading, fetchedData];
+};
